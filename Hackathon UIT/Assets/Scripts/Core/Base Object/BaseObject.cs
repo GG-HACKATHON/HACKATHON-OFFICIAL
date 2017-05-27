9ef6633eb4d64690c8c4a7ecf1481ae60e9b05c4
vthhanh96 <vthhanh96@gmail.com>
2017-05-27 11:22:44 +0700

    -core Base Object -core player Base Object -panda -panda animation

---
+++ Hackathon UIT/Assets/Scripts/Core/Base Object/BaseObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseObject : MonoBehaviour {
+    public Direction direction;
+
+    protected float health;
+
+    protected Animator anim;
+
+	public virtual void Init()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public virtual void OnHit(float dame)
+    {
+        health -= dame;
+        if (health <= 0)
+            Destroy(this.gameObject);
+    }
+}
---
+++ Hackathon UIT/Assets/Scripts/Core/Base Object/PlayerBaseObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBaseObject : BaseObject {
+
+    protected float occurTime;
+
+    protected float delayTime;
+
+    public virtual void OnAttack()
+    {
+    }
+}
